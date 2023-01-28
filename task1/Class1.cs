42ca1f700e7127c9a10ba523d2d74691f692ad7c
Pavel <sozonovps@gmail.com>
2023-01-28 18:22:03 +0800

    Добавьте файлы проекта.

---
+++ task1/Class1.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task1
+{
+    interface ISeries
+    {
+        void SetStart(int x);
+        int GetNext();
+        void Reset();
+    }
+
+    internal class ArithProgression : ISeries
+    {
+        public int Start { get; set; }
+        public int Step { get; set; }
+        public int CurrentValue { get; set; }
+        public int GetNext()
+        {
+            CurrentValue += Step;
+            return CurrentValue;
+        }
+        public ArithProgression(int initial, int step)
+        {
+            Start = initial;
+            CurrentValue = initial;
+            Step = step;
+        }
+        public ArithProgression()
+        {
+        }
+
+        public void Reset()
+        {
+            CurrentValue = Start;
+        }
+
+        public void SetStart(int x)
+        {
+            Start= x;
+        }
+    }
+    internal class GeomProgression : ISeries
+    {
+        public int Start { get; set; }
+        public int Step { get; set; }
+        public int CurrentValue { get; set; }
+        public int GetNext()
+        {
+            CurrentValue *= Step;
+            return CurrentValue;
+        }
+        public GeomProgression(int initial, int step)
+        {
+            Start = initial;
+            CurrentValue = initial;
+            Step = step;
+        }
+        public GeomProgression()
+        {
+        }
+
+        public void Reset()
+        {
+            CurrentValue = Start;
+        }
+
+        public void SetStart(int x)
+        {
+            Start = x;
+        }
+    }
+
+}
---
+++ task1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ArithProgression arith = new ArithProgression(1 , 2);
+            Console.WriteLine(arith.GetNext());
+            Console.WriteLine(arith.GetNext());
+            Console.WriteLine(arith.GetNext());
+            arith.Reset();
+            arith.Step = 5;
+            Console.WriteLine(arith.GetNext());
+            Console.WriteLine(arith.GetNext());
+            GeomProgression geom = new GeomProgression(1, 2);
+            Console.WriteLine(geom.GetNext());
+            Console.WriteLine(geom.GetNext());
+            Console.WriteLine(geom.GetNext());
+            geom.Reset();
+            geom.Step = 5;
+            Console.WriteLine(geom.GetNext());
+            Console.WriteLine(geom.GetNext());
+
+            Console.ReadKey();
+        }
+    }
+}
